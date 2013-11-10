41654ada39ad3eedfb144865e425da838f4691f0
Zhigang Chen <zhigangc@gmail.com>
2013-11-10 02:51:01 -0800

    debug bolt

--- bolt.go
+++ bolt.go
@@ -6,13 +6,17 @@ type Bolt interface {
 	Process(*Tuple)
 	Debug(string)
 	Ack(string)
+    Log(string)
 }
 
 func RunBolt(b Bolt) {
     b.Init("bolt")
     for {
+        b.Log("bolt.ReadTuple")
     	tup := b.ReadTuple()
+        b.Log("bolt.Process")
     	b.Process(tup)
+        b.Log("bolt.Ack")
     	b.Ack(tup.Id)
     }
 }
